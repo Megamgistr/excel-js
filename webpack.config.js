5b594fb2af1e441710e4233f236daec1cfddc5c5
nikita.barkov <Nikita.Barkov@nordigy.ru>
2020-07-13 15:06:52 +0300

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HTMLWebpachPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isProd = process.env.NODE_ENV === "production";
+
+const fileName = extensions =>
+  isProd ? `bundle.[hash].${extensions}` : `bundle.${extensions}`;
+
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"]
+      }
+    }
+  ];
+  if (!isProd) {
+    loaders.push("eslint-loader");
+  }
+  return loaders;
+};
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: ["@babel/polyfill", "./index.js"],
+  output: {
+    filename: fileName("js"),
+    path: path.resolve(__dirname, "dist")
+  },
+  resolve: {
+    extensions: [".js"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@core": path.resolve(__dirname, "src/core")
+    }
+  },
+  devtool: isProd ? false : "source-map",
+  devServer: {
+    port: 3000,
+    hot: !isProd
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpachPlugin({
+      template: "index.html",
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd
+      }
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/favicon.ico"),
+          to: path.resolve(__dirname, "dist")
+        }
+      ],
+      options: {
+        concurrency: 100
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: fileName("css")
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: !isProd,
+              reloadAll: true
+            }
+          },
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+        loader: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
